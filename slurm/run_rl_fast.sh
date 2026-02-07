eea1571c93b4f1f5bb3babdf165ab3d94e5ca86b
tomokazu-rikioka <tomokazu.rikioka@weblab.t.u-tokyo.ac.jp>
2026-02-08 01:03:55 +0900

    fix: export NVIDIA_DRIVER_CAPABILITIES before --nv to enable graphics capability

--- slurm/run_rl_fast.sh
+++ slurm/run_rl_fast.sh
@@ -11,6 +11,9 @@ cd ~/isaac-so-arm/gitrepo/isaac_so_arm101
 export JOB_TMP="${SLURM_TMPDIR:-$HOME/isaac-so-arm/tmp/$SLURM_JOB_ID}"
 mkdir -p "$JOB_TMP/IsaacLab"
 
+# nvidia-container-cli に graphics capability を有効にさせる（--nv 実行前に設定が必要）
+export NVIDIA_DRIVER_CAPABILITIES=all
+
 # コンテナ内で動作する Vulkan ICD JSON を生成
 mkdir -p "$JOB_TMP/vulkan/icd.d"
 cat > "$JOB_TMP/vulkan/icd.d/nvidia_icd.json" <<'VICD'
@@ -37,7 +40,6 @@ singularity exec --nv --writable-tmpfs \
   --bind /usr/share/glvnd/egl_vendor.d/10_nvidia.json:/usr/share/glvnd/egl_vendor.d/10_nvidia.json:ro \
   --env VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json \
   --env VK_DRIVER_FILES=/usr/share/vulkan/icd.d/nvidia_icd.json \
-  --env NVIDIA_DRIVER_CAPABILITIES=all \
   ${WANDB_API_KEY:+--env WANDB_API_KEY="$WANDB_API_KEY"} \
   ~/isaac-so-arm/containers/isaac-lab.sif \
   uv run src/isaac_so_arm101/scripts/rsl_rl/train.py \
